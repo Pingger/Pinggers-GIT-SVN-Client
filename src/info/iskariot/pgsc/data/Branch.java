4cf8135483a94acf2d69b2aed2ba267b91a12d67
Pingger Shikkoken <pingger@iskariot.info>
2018-02-03 15:20:12 +0100

    Added getParent() to Branch

--- src/info/iskariot/pgsc/data/Branch.java
+++ src/info/iskariot/pgsc/data/Branch.java
@@ -15,6 +15,9 @@ public interface Branch
 	/** @return the Label of the Branch */
 	public String getLabel();
 	
+	/** @return the Parent Branch. Should return null if this the master/HEAD Branch */
+	public Branch getParent();
+	
 	/** @return the Project to which the Branch belongs */
 	public Project getProject();
 }
